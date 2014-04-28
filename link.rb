66e9357da2ecec0d31567b829e02db2499ba192a
REsessh <info@resessh.net>
2014-04-28 21:04:54 +0900

    adds link.rb to make symbolic link automatically.

---
+++ link.rb
@@ -0,0 +1,22 @@
+#!/home/resessh/.rbenv/shims/ruby
+
+ignoreFiles = [".git", ".", "..", ".gitignore", "README.rb", "link.rb"]
+
+currentDir = Dir::getwd
+homeDir = File::dirname(currentDir)
+
+
+dirFiles = Dir::entries(currentDir)
+homedirFiles = Dir::entries(homeDir)
+dirFiles.each do |dirfile|
+	if ignoreFiles.include?(dirfile)
+		puts dirfile+" is ignored."
+	else
+		if homedirFiles.include?(dirfile)
+			puts dirfile+" is already exists."
+		else
+			puts "make symbolick link of "+dirfile+"." 
+			File::symlink( currentDir+"/"+dirfile, homeDir+"/"+dirfile)
+		end
+	end
+end

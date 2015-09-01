6d5d5b713293970896c3c24d018df4c4bed78227
REsessh <info@resessh.net>
2015-09-02 01:47:18 +0900

    :art: 微修正 & linkをシェルスクリプトに変更

---
+++ link.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CRNTDIR=$(cd $(dirname $0) && pwd)
+
+ln -s CRNTDIR/.zshrc ~/.
+ln -s CRNTDIR/.zshrc_prompt ~/.
+ln -s CRNTDIR/.dotfiles/.zshrc_path ~/.
+ln -s CRNTDIR/.zshrc_alias ~/.
+cp CRNTDIR/.zshrc_local ~/.
+
+ln -s CRNTDIR/.vimrc ~/.vimrc
+
+ln -s CRNTDIR/.tmux.conf ~/.
+cp CRNTDIR/.tmux.local.conf ~/.
+
+ln -s CRNTDIR/.gitconfig ~/.

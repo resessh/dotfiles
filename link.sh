b0524635a32b83771dbb56c0218c697f62e20efa
REsessh <info@resessh.net>
2015-09-02 02:10:29 +0900

    :bug: link.shの不具合を修正

--- link.sh
+++ link.sh
@@ -2,15 +2,15 @@
 
 CRNTDIR=$(cd $(dirname $0) && pwd)
 
-ln -s CRNTDIR/.zshrc ~/.
-ln -s CRNTDIR/.zshrc_prompt ~/.
-ln -s CRNTDIR/.dotfiles/.zshrc_path ~/.
-ln -s CRNTDIR/.zshrc_alias ~/.
-cp CRNTDIR/.zshrc_local ~/.
+ln -s $CRNTDIR/.zshrc ~/.
+ln -s $CRNTDIR/.zshrc_prompt ~/.
+ln -s $CRNTDIR/.zshrc_path ~/.
+ln -s $CRNTDIR/.zshrc_alias ~/.
+cp $CRNTDIR/.zshrc_local ~/.
 
-ln -s CRNTDIR/.vimrc ~/.vimrc
+ln -s $CRNTDIR/.vimrc ~/.vimrc
 
-ln -s CRNTDIR/.tmux.conf ~/.
-cp CRNTDIR/.tmux.local.conf ~/.
+ln -s $CRNTDIR/.tmux.conf ~/.
+cp $CRNTDIR/.tmux.local.conf ~/.
 
-ln -s CRNTDIR/.gitconfig ~/.
+ln -s $CRNTDIR/.gitconfig ~/.
